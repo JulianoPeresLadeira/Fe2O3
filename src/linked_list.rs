b5f308c40956bd789df4f71d6d0f24db7a07df03
Juliano Ladeira <julianoladeira@gmail.com>
2023-03-22 20:01:17 +0100

    Added simple linked list implementation

---
+++ src/linked_list.rs
@@ -0,0 +1,41 @@
+struct Node {
+    val: i32,
+    next: Option<Box<Node>>
+}
+
+impl Node {
+    pub fn new(value: i32) -> Self {
+        Self {
+            val: value,
+            next: None
+        }
+    }
+
+    pub fn print(&self) {
+        println!("{}", self.val);
+
+        match &self.next {
+            None => {},
+            Some(n) => n.print()
+        }
+    }
+
+    pub fn append(&mut self, new_value: i32) {
+        match &mut self.next {
+            None => self.next = Some(Box::new(Node::new(new_value))),
+            Some(n) => n.append(new_value),
+        }
+    }
+}
+
+fn main() {
+    let mut x = Node::new(1);
+    x.append(2);
+    x.append(3);
+    x.append(4);
+    x.append(5);
+    x.append(6);
+    x.append(7);
+    x.append(8);
+    x.print();
+}
