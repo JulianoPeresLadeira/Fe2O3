70dd73423130c20b05dafea4e922c4a006dab9a8
Juliano Ladeira <julianoladeira@gmail.com>
2023-03-23 23:57:12 +0100

    added a clear and a remove at index function to linked list

--- src/linked_list.rs
+++ src/linked_list.rs
@@ -41,22 +41,56 @@ impl LinkedList {
         self.length += 1;
     }
 
+    pub fn clear(&mut self) {
+        self.head = None;
+        self.length = 0;
+    }
+
     pub fn print(&self) {
+        let mut curr_index = 0;
         let mut curr = &self.head;
         while curr.is_some() {
             let curr_node = curr.as_ref().unwrap();
-            println!("{}", curr_node.val);
+            println!("{}: \t{}", curr_index, curr_node.val);
+            curr_index += 1;
             curr = &curr_node.next;
         }
+    }
+
+    pub fn remove_at_index(&mut self, index: u32) {
+        if index >= self.length {
+            return;
+        }
+
+        if index == 0 {
+            self.head = self.head.as_mut().unwrap().next.take();
+        } else {
+            let mut curr :&mut Option<Box<Node>> = &mut self.head;
+            let mut helper_index = 1;
+
+            while helper_index < index {
+                curr = &mut curr.as_mut().unwrap().next;
+                helper_index += 1;
+            }
+
+            curr.as_mut().unwrap().next = curr.as_mut().unwrap().next.as_mut().unwrap().next.take();
+        }
+
+        self.length -= 1;
     }
 }
 
 fn main() {
     let mut list = LinkedList::new();
+    list.append(0);
     list.append(1);
     list.append(2);
     list.append(3);
     list.append(4);
     list.append(5);
+
+    list.print();
+    println! ("");
+    list.remove_at_index(1);
     list.print();
 }
